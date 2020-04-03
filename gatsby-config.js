d9043886009bcf5cdff22d0a91e222d51d480a50
Max Mayorov <max.mayorov@boondoggle.eu>
2020-04-03 23:42:54 +0200

    Token to env.local

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `19bf77a424297e46168de7b1b7a3ec`,
+        preview: false,
+        disableLiveReload: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/pages/cv.js
@@ -0,0 +1,29 @@
+import React from 'react';
+const Cv = ({ data }) => (
+  <article className="sheet">
+    <pre>{JSON.stringify(data)}</pre>
+    <h1>{data.cv.name}</h1>
+    <p>{data.cv.motto}</p>
+    <img src={data.cv.photo.url} />
+    <div dangerouslySetInnerHTML={{ __html: data.cv.description }} />
+  </article>
+);
+export default Cv;
+export const query = graphql`
+query CvQuery {
+  cv: datoCmsCv {
+    motto
+    name
+    description
+    photo {
+      url
+    }
+  }
+  skillset: datoCmsSkillset {
+    title
+    skills {
+      title
+    }
+  }
+}
+`;
